667b3cf643b27abbe348700fbc4ebf6529f5976c
MichaelJAlvarez <alvarez.michael.joseph@gmail.com>
2020-05-23 11:16:54 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { of, fromEvent } from 'rxjs'; 
+import {Observable} from 'rxjs/Observable';
+import { concatAll, map, takeUntil } from 'rxjs/operators';
+import './style.css';
+
+const container = document.getElementById('parent');
+const draggable = document.getElementById('draggable');
+
+// Events
+const mouseDowns$ = fromEvent(document, 'mousedown');
+const mouseMoves$ = fromEvent(document, 'mousemove');
+const mouseUps$ = fromEvent(document, 'mouseup');
+
+const drags = mouseDowns$.pipe(
+  map((x) => {
+    return mouseMoves$.pipe(takeUntil(mouseUps$))
+  }),
+  concatAll()
+);
+
+
+drags.forEach((e) => {
+  draggable.style.left = e.clientX + 'px';
+  draggable.style.top = e.clientY + 'px';
+})
